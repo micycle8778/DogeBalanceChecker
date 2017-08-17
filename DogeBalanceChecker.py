1f9a0f824f5aa492f1d9584c8989aea84dc87d20
Felix Zactor <buildermancampbell@aol.com>
2017-08-17 00:10:12 -0400

    Multicurrency update 1.2 unstable
    Bitcoin and Litecoin support coming soon!

--- DogeBalanceChecker.py
+++ DogeBalanceChecker.py
@@ -23,23 +23,54 @@ Help:
 -h : display help text
 
 -v : display the program version
+
+-B : display balance of bitcoin addresses
+
+-L : display balance of litecoin addresses
 """
 version = "DogeBalanceChecker 1.1"
 
 coinmarketcap = Market()
-dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="USD")[0]
-usdprice = dogecoin["price_usd"]
-btcprice = dogecoin["price_btc"]
-dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="EUR")[0]
-eurprice = dogecoin["price_eur"]
-dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="GBP")[0]
-gbpprice = dogecoin["price_gbp"]
-dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="AUD")[0]
-audprice = dogecoin["price_aud"]
-dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="CAD")[0]
-cadprice = dogecoin["price_cad"]
+class doge:
+	dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="USD")[0]
+	usdprice = dogecoin["price_usd"]
+	btcprice = dogecoin["price_btc"]
+	dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="EUR")[0]
+	eurprice = dogecoin["price_eur"]
+	dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="GBP")[0]
+	gbpprice = dogecoin["price_gbp"]
+	dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="AUD")[0]
+	audprice = dogecoin["price_aud"]
+	dogecoin = coinmarketcap.ticker("Dogecoin", limit=3, convert="CAD")[0]
+	cadprice = dogecoin["price_cad"]
 
-def balance():
+class btc:
+	bitcoin = coinmarketcap.ticker("Bitcoin", limit=3, convert="USD")[0]
+	usdprice = bitcoin["price_usd"]
+	btcprice = bitcoin["price_btc"]
+	bitcoin = coinmarketcap.ticker("Bitcoin", limit=3, convert="EUR")[0]
+	eurprice = bitcoin["price_eur"]
+	bitcoin = coinmarketcap.ticker("Bitcoin", limit=3, convert="GBP")[0]
+	gbpprice = bitcoin["price_gbp"]
+	bitcoin = coinmarketcap.ticker("Bitcoin", limit=3, convert="AUD")[0]
+	audprice = bitcoin["price_aud"]
+	bitcoin = coinmarketcap.ticker("Bitcoin", limit=3, convert="CAD")[0]
+	cadprice = bitcoin["price_cad"]
+
+class ltc:
+	litecoin = coinmarketcap.ticker("Litecoin", limit=3, convert="USD")[0]
+	usdprice = litecoin["price_usd"]
+	btcprice = litecoin["price_btc"]
+	litecoin = coinmarketcap.ticker("Litecoin", limit=3, convert="EUR")[0]
+	eurprice = litecoin["price_eur"]
+	litecoin = coinmarketcap.ticker("Litecoin", limit=3, convert="GBP")[0]
+	gbpprice = litecoin["price_gbp"]
+	litecoin = coinmarketcap.ticker("Litecoin", limit=3, convert="AUD")[0]
+	audprice = litecoin["price_aud"]
+	litecoin = coinmarketcap.ticker("Litecoin", limit=3, convert="CAD")[0]
+	cadprice = litecoin["price_cad"]
+
+def dogebalance():
 	os.system('cls' if os.name == 'nt' else 'clear')
 	balance = []
 	for i in addresses:
@@ -56,6 +87,42 @@ def balance():
 	print("balance eur :", str(float(eurprice) * totalBalance))
 	print("balance aud :", str(float(audprice) * totalBalance))
 	print("balance cad :", str(float(cadprice) * totalBalance))
+	
+def btcbalance():
+	os.system('cls' if os.name == 'nt' else 'clear')
+	balance = []
+	for i in addresses:
+		get_address_info = requests.get('https://api.blockcypher.com/v1/btc/main/addrs/'+i+'/full?limit=99999')
+		address_info = get_address_info.text
+		j_address_info = json.loads(address_info)
+		balance.append(j_address_info['balance'])
+		print('address : '+str(i)+' - balance : '+str(j_address_info['balance']/100000000)+' btc')
+	print('total balance : '+str(sum(balance)/100000000)+' btc')
+	totalBalance = sum(balance)/100000000
+	print("balance usd :", str(float(usdprice) * totalBalance))
+	print("balance btc :", str(float(btcprice) * totalBalance))
+	print("balance gbp :", str(float(gbpprice) * totalBalance))
+	print("balance eur :", str(float(eurprice) * totalBalance))
+	print("balance aud :", str(float(audprice) * totalBalance))
+	print("balance cad :", str(float(cadprice) * totalBalance))
+	
+def ltcbalance():
+	os.system('cls' if os.name == 'nt' else 'clear')
+	balance = []
+	for i in addresses:
+		get_address_info = requests.get('https://api.blockcypher.com/v1/ltc/main/addrs/'+i+'/full?limit=99999')
+		address_info = get_address_info.text
+		j_address_info = json.loads(address_info)
+		balance.append(j_address_info['balance'])
+		print('address : '+str(i)+' - balance : '+str(j_address_info['balance']/100000000)+' ltc')
+	print('total balance : '+str(sum(balance)/100000000)+' ltc')
+	totalBalance = sum(balance)/100000000
+	print("balance usd :", str(float(usdprice) * totalBalance))
+	print("balance btc :", str(float(btcprice) * totalBalance))
+	print("balance gbp :", str(float(gbpprice) * totalBalance))
+	print("balance eur :", str(float(eurprice) * totalBalance))
+	print("balance aud :", str(float(audprice) * totalBalance))
+	print("balance cad :", str(float(cadprice) * totalBalance))
 
 def lookup(address):
 	loop = True
@@ -65,25 +132,25 @@ def lookup(address):
 		os.system('cls' if os.name == 'nt' else 'clear')
 		balance = []
 		for i in addresses:
-			get_address_info = requests.get('https://api.blockcypher.com/v1/doge/main/addrs/'+i+'/full?limit=99999')
+			get_address_info = requests.get('https://api.blockcypher.com/v1/'+currency+'/main/addrs/'+i+'/full?limit=99999')
 			address_info = get_address_info.text
 			j_address_info = json.loads(address_info)
 			balance.append(j_address_info['balance'])
 		print(addresses[0]+"'s balance : "+str(sum(balance)/100000000)+' doge')
 		totalBalance = sum(balance)/100000000
-		print("balance usd :", str(float(usdprice) * totalBalance))
-		print("balance btc :", str(float(btcprice) * totalBalance))
-		print("balance gbp :", str(float(gbpprice) * totalBalance))
-		print("balance eur :", str(float(eurprice) * totalBalance))
-		print("balance aud :", str(float(audprice) * totalBalance))
-		print("balance cad :", str(float(cadprice) * totalBalance))
+		print("balance usd :", str(float(doge.usdprice) * totalBalance))
+		print("balance btc :", str(float(doge.btcprice) * totalBalance))
+		print("balance gbp :", str(float(doge.gbpprice) * totalBalance))
+		print("balance eur :", str(float(doge.eurprice) * totalBalance))
+		print("balance aud :", str(float(doge.audprice) * totalBalance))
+		print("balance cad :", str(float(doge.cadprice) * totalBalance))
 		if input("Do you want to search another address(Y/n)? ").lower() == "n":
 			loop = False
 			break
 		address = [input("What address balance do you want to lookup? ")]
 
-def importAddresses():
-	addresses = open("addresses.txt", "r")
+def importAddresses(currency="addresses"):
+	addresses = open(currency+".txt", "r")
 	x = []
 	for line in addresses:
 		x.append(line)
@@ -94,8 +161,8 @@ def importAddresses():
 	addresses = x
 	return addresses
 	
-def addAddress(address):
-	addresses = open("addresses.txt", "r+")
+def addAddress(address, currency="addresses"):
+	addresses = open(currency+".txt", "r+")
 	x = importAddresses()
 	for i in range(len(x)):
 		addresses.write(x[i] + '\n')
@@ -110,19 +177,21 @@ def main(flag):
 		elif flag[0] == "-a":
 			try:
 				addAddress(flag[1])
-			except:
+			except IndexError:
 				print(helptext)
-				raise ValueError("Usage: -a [ADDRESS]")
 		elif flag[0] == "-l":
 			try:
 				lookup(flag[1])
-			except:
+			except IndexError:
 				print(helptext)
-				raise ValueError("Usage: -l [ADDRESS]")
 		elif flag[0] == "-b":
 			balance()
 		elif flag[0] == "-v":
 			print(version)
+		elif flag[0] == "-B":
+			balance(currency="btc")
+		elif flag[0] == "-L":
+			balance(currency="ltc")
 		else:
 			print(helptext)
 	except IndexError:
@@ -130,5 +199,4 @@ def main(flag):
 	
 
 if __name__ == "__main__":
-	addresses = importAddresses()
 	main(sys.argv[1:])
